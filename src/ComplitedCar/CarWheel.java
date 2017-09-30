4c51bc0fdaf10c8b0f9ac17c0c6f8a8f9e63af49
BohdanRadchuk <shpionuse@gmail.com>
2017-09-30 20:01:51 +0300

    wheel

--- src/ComplitedCar/CarWheel.java
+++ src/ComplitedCar/CarWheel.java
@@ -1,4 +1,34 @@
 package ComplitedCar;
 
 public class CarWheel {
+    private float wheelerasuer;
+    public CarWheel (){
+        this.wheelerasuer = 0.85f;
+    }
+    public  CarWheel (float wheelerasuer){
+        this.wheelerasuer = wheelerasuer;
+    }
+
+
+    public float getWheelerasuer() {
+        return wheelerasuer;
+    }
+    public void setWheelerasuer(float wheelerasuer) {
+        this.wheelerasuer = wheelerasuer;
+    }
+    public void getNewTire (){
+        setWheelerasuer(1f);
+    }
+    public  void  tireUsage (float usage){
+        this.wheelerasuer = wheelerasuer - usage;
+    }
+    public void  show (){
+        if (wheelerasuer>0 && wheelerasuer<=1)
+        System.out.println("шина цела на "+ wheelerasuer*100 +"%");
+        else {
+            if (wheelerasuer <= 0)
+                System.out.println("шина полностью стрёрта");
+            else System.out.println("это уже не одна шина");
+        }
+    }
 }
